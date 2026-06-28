5eafc0b4a7b28a3acedc4353b38ac0c30a928693
Wojtek Gorzawski <wojtek.gorzawski@gmail.com>
2026-06-28 15:02:36 +0200

    feat: add SVG favicon based on app logo

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
         {
